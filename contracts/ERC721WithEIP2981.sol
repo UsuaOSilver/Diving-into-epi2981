ba007b8b16cac0bd1f7b184e030faac4ed6dafc6
N. Anh Nguyen <48362877+UsuaOSilver@users.noreply.github.com>
2022-10-15 11:39:34 -0700

    Update ERC721WithEIP2981.sol

--- contracts/ERC721WithEIP2981.sol
+++ contracts/ERC721WithEIP2981.sol
@@ -11,7 +11,7 @@ contract ERC721WithEIP2981 is ERC721, ERC2981Base {
   uint256 newTokenId;
   
   /**
-   * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
+   * @dev Initializes the contract.
    */
   constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
   
@@ -19,11 +19,35 @@ contract ERC721WithEIP2981 is ERC721, ERC2981Base {
    * @dev See {IERC165-supportsInterface}.
    */
   function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC2981)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+      public
+      view
+      override(ERC721, ERC2981)
+      returns (bool)
+  {
+      return super.supportsInterface(interfaceId);
+  }
+  
+   /**
+   * @notice Sets the contract-wide royalty info.
+   * @dev This function should implement onlyOwner modifier in production.
+   * @param receiver        The royalties recipient
+   * @param feeBasisPoints  Royalties value 
+   */
+  function setRoyaltyInfo(address receiver, uint96 feeBasisPoints)
+      external
+      /*onlyOwner*/
+  {
+      _setDefaultRoyalty(receiver, feeBasisPoints);
+  }
+  
+  /**
+   * @notice   Mint one token to address '_to'
+   * @param to The address receiving the newly minted token
+   */
+  function mint(address _to) external {
+      uint256 _tokenId = newTokenId;
+      _safeMint(to, _tokenId, '');
+
+      newTokenId = _tokenId + 1;
+  }
 }
